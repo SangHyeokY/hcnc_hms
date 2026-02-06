71a81e89646ad303f7c738d41166e8a33eda702e
SangHyeokY <ush529@gmail.com>
2026-02-06 09:38:59 +0900

    [Fix] 인적사항 주석 추가

--- hcnc_init/src/main/java/com/hcncinit/service/hr010/Hr010Service.java
+++ hcnc_init/src/main/java/com/hcncinit/service/hr010/Hr010Service.java
@@ -1,9 +1,7 @@
 package com.hcncinit.service.hr010;
 
 import com.hcncinit.logging.QryLog;
-import java.io.InputStream;
 import java.util.List;
-import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,37 +13,40 @@ public class Hr010Service {
     @Autowired
     private SqlSession sqlSession;
 
-    // 인력관리 > 기본 인적사항 (임시/검색x), 따로 분류할건지는 논의
+    // [인적관리] - [기본 인적사항]
+
+    // [기본 인적사항] > 조회
     @QryLog(scrnCd = "HR010", fnCd = "DEV_LIST", opTyp = "SELECT")
     public List<Map<String, Object>> select_hr010(Map<String, Object> map) {
         return this.sqlSession.selectList("com.hcncinit.Hr010Mapper.select_hr010", map);
     }
 
-    // 인력관리 > 기본 인적사항 이미지
+    // [기본 인적사항] > 이미지
     @QryLog(scrnCd = "HR010", fnCd = "DEV_IMG", opTyp = "SELECT")
     public byte[] select_hr010_img(Map<String, Object> map) {
         byte[] res = this.sqlSession.selectOne("com.hcncinit.Hr010Mapper.select_hr010_img", map);
         return res;
     }
 
-    // 인력관리 신규 등록/수정
+    // [기본 인적사항] > 신규 등록/수정
     @QryLog(scrnCd = "HR010", fnCd = "DEV_SAVE", opTyp = "UPSERT")
     public int insert_hr010(Map<String, Object> map) {
         return this.sqlSession.insert("com.hcncinit.Hr010Mapper.insert_hr010", map);
     }
 
-    // 인력관리 => dev_id 생성
+    // [기본 인적사항] > dev_id 생성
     @QryLog(scrnCd = "HR010", fnCd = "DEV_GEN_ID", opTyp = "SELECT", logParams = false)
     public String generateDevId(String devType) {
         return this.sqlSession.selectOne("com.hcncinit.Hr010Mapper.selectDevId", devType);
     }
 
-    // 인력관리 삭제
+    // [기본 인적사항] > 삭제
     @QryLog(scrnCd = "HR010", fnCd = "DEV_DELETE", opTyp = "DELETE")
     public int delete_hr010(Map<String, Object> map) {
         return this.sqlSession.update("com.hcncinit.Hr010Mapper.delete_hr010", map);
     }
 
+    // [기본 인적사항] > 등급 계산
     @QryLog(scrnCd = "HR010", fnCd = "DEV_SCORE", opTyp = "SELECT")
     public Map<String, Object> dev_score(String devId) {
         return this.sqlSession.selectOne("com.hcncinit.Hr010Mapper.dev_score", devId);
--- hcnc_init/src/main/java/com/hcncinit/service/hr010/Hr011Service.java
+++ hcnc_init/src/main/java/com/hcncinit/service/hr010/Hr011Service.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 @Service("Hr011Service")
@@ -14,19 +13,21 @@ public class Hr011Service {
     @Autowired
     private SqlSession sqlSession;
 
-    // tab1 조회
+    // [인적관리] - [기본 인적사항] - [Tab1][소속 및 계약정보]
+
+    // [Tab1][소속 및 계약정보] > 조회
     @QryLog(scrnCd = "HR010", fnCd = "TAB1_SELECT", opTyp = "SELECT")
     public Map<String, Object> select_tab1(String devId) {
         return this.sqlSession.selectOne("com.hcncinit.Hr011Mapper.select_tab1", devId);
     }
 
-    // tab 등록/수정
+    // [Tab1][소속 및 계약정보] > 등록/수정
     @QryLog(scrnCd = "HR010", fnCd = "TAB1_SAVE", opTyp = "UPSERT")
     public int upsert_tab1(Map<String, Object> map) {
         return this.sqlSession.insert("com.hcncinit.Hr011Mapper.upsert_tab1", map);
     }
 
-    // tab 등록/수정
+    // [Tab1][소속 및 계약정보] > 삭제
     @QryLog(scrnCd = "HR010", fnCd = "TAB1_DELETE", opTyp = "DELETE")
     public int delete_tab1(Map<String, Object> map) {
         return this.sqlSession.update("com.hcncinit.Hr011Mapper.delete_tab1", map);
